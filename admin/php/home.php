2b12809e80969b39ea21fdae9fed0166dfdac90d
Vu tran <levu1962004@gmail.com>
2023-09-03 12:21:54 +0700

    nháp

--- admin/php/home.php
+++ admin/php/home.php
@@ -1,15 +1,19 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/admin/css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="/admin/css/home.css">
+    <link rel="stylesheet" href="../css/home.css">
 </head>
+
 <body>
     <div class="main">
         <header>
@@ -17,7 +21,7 @@
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid" style="height: 90px;">
                     <a class="navbar-brand" href="#">
-                        <img src="/public/images/black-green-futuristic-game-logo-1.png" width="100" height="90" alt="">
+                        <img src="./images/logo/black-green-futuristic-game-logo-1.png" width="100" height="90" alt="">
                     </a>
                     <div class="container-fluid d-flex">
                         <form class="d-flex" role="search">
@@ -42,19 +46,19 @@
             <nav class="nav navbar  navbar-light" style="background-color: #625D5D; padding: 0;">
                 <ul class="nav" style="margin-left: 40px;">
                     <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" href="#">Active</a>
+                        <a class="nav-link active" aria-current="page" href="#">Active</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Link</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Link</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link " href="#" >Disabled</a>
+                        <a class="nav-link " href="#">Disabled</a>
                     </li>
-                  </ul>
-              </nav>
+                </ul>
+            </nav>
         </header>
     </div>
 
@@ -65,50 +69,45 @@
                     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-inner">
                             <div class="carousel-item active" data-bs-interval="10000">
-                                <img src="/images (1)/game/1.png" class="d-block w-100" style="border-radius: 10px;"
-                                    alt="...">
+                                <img src="./images/add/10.jpg" class="d-block w-100" style="border-radius: 10px;" alt="...">
                             </div>
                             <div class="carousel-item" data-bs-interval="2000">
-                                <img src="/images (1)/game/1.png" class="d-block w-100" style="border-radius: 10px;"
-                                    alt="...">
+                                <img src="./images/game/1.png" class="d-block w-100" style="border-radius: 10px;" alt="...">
                             </div>
                             <div class="carousel-item">
-                                <img src="/images (1)/game/1.png" class="d-block w-100" style="border-radius: 10px;"
-                                    alt="...">
+                                <img src="./images/office/11.jpg" class="d-block w-100" style="border-radius: 10px;" alt="...">
                             </div>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
-                            data-bs-slide="prev">
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
-                            data-bs-slide="next">
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
                     </div>
                 </div>
-    
+
                 <div class="section-slider-right">
                     <div class="section-slider-img">
-                        <img src="/images (1)/office/12.jpg" alt="">
+                        <img src="./images/office/12.jpg" alt="">
                     </div>
                     <div class="section-slider-img">
-                        <img src="/images (1)/add/9.jpg" alt="">
+                        <img src="./images/add/9.jpg" alt="">
                     </div>
                 </div>
             </div>
-    
+
             <div class="section-slider-bottom">
                 <div class="section-slider-bootom-img">
-                    <img src="/images (1)/game/6.png" alt="">
+                    <img src="./images/game/6.png" alt="">
                 </div>
                 <div class="section-slider-bootom-img">
-                    <img src="/images (1)/game/1.png" alt="">
+                    <img src="./images/game/1.png" alt="">
                 </div>
                 <div class="section-slider-bootom-img">
-                    <img src="/images (1)/game/2.png" alt="">
+                    <img src="./images/game/2.png" alt="">
                 </div>
             </div>
         </div>
@@ -149,7 +148,8 @@
                             content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                </div><div class="card" style="width: 18rem; margin-bottom:20px;">
+                </div>
+                <div class="card" style="width: 18rem; margin-bottom:20px;">
                     <img src="/images (1)/add/10.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
@@ -157,7 +157,8 @@
                             content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                </div><div class="card" style="width: 18rem; margin-bottom:20px;">
+                </div>
+                <div class="card" style="width: 18rem; margin-bottom:20px;">
                     <img src="/images (1)/add/10.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
@@ -165,7 +166,8 @@
                             content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                </div><div class="card" style="width: 18rem; margin-bottom:20px;">
+                </div>
+                <div class="card" style="width: 18rem; margin-bottom:20px;">
                     <img src="/images (1)/add/10.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
@@ -186,58 +188,26 @@
             </div>
 
             <div class="carts">
-                <div class="card" style="width: 18rem; margin-bottom:20px;">
-                    <img src="/images (1)/add/10.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
 
-                <div class="card" style="width: 18rem; margin-bottom:20px;">
-                    <img src="/images (1)/add/10.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                <?php
+                $sql = "SELECT id,image,name,price FROM products WHERE category_id=3 AND status = 0";
+                $result = mysqli_query($conn, $sql);
+
+                while ($kq = mysqli_fetch_assoc($result)) {
+                ?>
+                    <div class="card" style="width: 18rem; margin-bottom:20px;">
+                        <img src="<?php echo $kq['image']; ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $kq['name']; ?></h5>
+                            <p class="card-text"><?php echo $kq['price']; ?><sup> đ</sup></p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
                     </div>
-                </div>
-                <div class="card" style="width: 18rem; margin-bottom:20px;">
-                    <img src="/images (1)/add/10.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div><div class="card" style="width: 18rem; margin-bottom:20px;">
-                    <img src="/images (1)/add/10.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div><div class="card" style="width: 18rem; margin-bottom:20px;">
-                    <img src="/images (1)/add/10.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div><div class="card" style="width: 18rem; margin-bottom:20px;">
-                    <img src="/images (1)/add/10.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+
+                <?php } ?>
+
+
 
             </div>
         </div>
@@ -250,7 +220,7 @@
                 <div class="col-md-3 col-lg-3 col-xl-3 mx-auti mt-3">
                     <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Company Name</h5>
                     <p>Get ready to show off your love for Minecraft with officially curated Minecraft apparel, accessories, and drinkware.</p>
-                    
+
                 </div>
                 <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
                     <h5 class="text-uppercase mb-4 font-weight-bold text-warning"> Products </h5>
@@ -294,7 +264,7 @@
                     </p>
                     <p>
                         <i class="fas fa-phone mr-3"></i> 0906472426
-                    </p>    
+                    </p>
                     <p>
                         <i class="fas fa-print mr-3"></i> 0926688124
                     </p>
@@ -336,4 +306,5 @@
         </div>
     </footer>
 </body>
+
 </html>
