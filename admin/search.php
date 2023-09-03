efb01fcd340a07481d48061faac2a7db9434a9df
Vu tran <levu1962004@gmail.com>
2023-09-03 18:39:50 +0700

    update search

--- admin/search.php
+++ admin/search.php
@@ -1,7 +1,24 @@
 <?php
-require_once '../model/connect.php';
+require_once('../model/connect.php');
+$render = "";
+if (isset($_POST['name-product'])) {
+    $query = "SELECT * from products where name like '%" . $_POST['name-product'] . "%'";
+    $result = mysqli_query($conn, $query);
+    if (mysqli_num_rows($result)) {
+        while ($table = mysqli_fetch_array($result)) {
+            $render .= '
+            <div class="card" style="width: 18rem; margin-bottom:20px;">
+            <img src="'.$table['image'].'" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">'.$table['name'].'</h5>
+                <p class="card-text">'.$table["price"].'</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+            ';}
+}
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -26,10 +43,11 @@ require_once '../model/connect.php';
                         <img src="./images/logo/black-green-futuristic-game-logo-1.png" width="100" height="90" alt="">
                     </a>
                     <div class="container-fluid d-flex">
-                        <form class="d-flex" action="search.php" method="POST">
-                            <input class="form-control me-2" style="margin-left: 30px;width: 300px;" name="name-product" placeholder="What do you need?" aria-label="Search">
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" style="margin-left: 30px;width: 300px;" placeholder="Search" aria-label="Search">
                             <button class="btn btn-outline-success" type="submit">Search</button>
                         </form>
+
                     </div>
                     <div class="login container-fluid " style="margin-left: 300px;">
                         <i class="fa-solid fa-user"></i>
@@ -64,135 +82,68 @@ require_once '../model/connect.php';
     </div>
 
     <div class="slider">
-        <div class="content">
-            <div class="section">
-                <div class="section-slider-left">
-                    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active" data-bs-interval="10000">
-                                <img src="./images/add/10.jpg" class="d-block w-100" style="border-radius: 10px;" alt="...">
-                            </div>
-                            <div class="carousel-item" data-bs-interval="2000">
-                                <img src="./images/game/1.png" class="d-block w-100" style="border-radius: 10px;" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="./images/office/11.jpg" class="d-block w-100" style="border-radius: 10px;" alt="...">
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
-                </div>
-
-                <div class="section-slider-right">
-                    <div class="section-slider-img">
-                        <img src="./images/office/12.jpg" alt="">
-                    </div>
-                    <div class="section-slider-img">
-                        <img src="./images/add/9.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-
-            <div class="section-slider-bottom">
-                <div class="section-slider-bootom-img">
-                    <img src="./images/game/6.png" alt="">
-                </div>
-                <div class="section-slider-bootom-img">
-                    <img src="./images/game/1.png" alt="">
-                </div>
-                <div class="section-slider-bootom-img">
-                    <img src="./images/game/2.png" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="slider">
         <div class="contents">
             <div class="title">
-                <h2>Window Bản Quyền</h2>
+                <h2>Tìm kiếm sản phẩm</h2>
             </div>
-            <div class="carts">
-                <?php
-                $sql = "SELECT id,image,name,price FROM products WHERE category_id=1 AND status = 0";
-                $result = mysqli_query($conn, $sql);
+            <div class="search-bar">
+                <tr>
+                    <td class="tdLabel"><label for="register_country" class="label">Danh mục: </label></td>
+                    <td>
+                      <select name="country" id="register_country" style="width:160px">
+                        <option value="india">india</option>
+                        <option value="pakistan">pakistan</option>
+                        <option value="africa">africa</option>
+                        <option value="china">china</option>
+                        <option value="other">other</option>
+                      </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="tdLabel"><label for="register_country" class="label">Thể loại: </label></td>
+                    <td>
+                      <select name="country" id="register_country" style="width:160px">
+                        <option value="india">Tất cả</option>
+                        <option value="pakistan">pakistan</option>
+                        <option value="africa">africa</option>
+                        <option value="china">china</option>
+                        <option value="other">other</option>
+                      </select>
+                    </td>
+                </tr>
 
-                while ($kq = mysqli_fetch_assoc($result)) {
-                ?>
-                    <div class="card" style="width: 18rem; margin-bottom:20px;">
-                        <img src="<?php echo $kq['image']; ?>" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $kq['name']; ?></h5>
-                            <p class="card-text"><?php echo $kq['price']; ?><sup> đ</sup></p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-
-                <?php } ?>
+                <tr>
+                    <td class="tdLabel"><label for="register_country" class="label">Mức giá: </label></td>
+                    <td>
+                      <select name="country" id="register_country" style="width:160px">
+                        <option value="india">Tất cả</option>
+                        <option value="pakistan">pakistan</option>
+                        <option value="africa">africa</option>
+                        <option value="china">china</option>
+                        <option value="other">other</option>
+                      </select>
+                    </td>
+                    <tr>
+                        <td class="tdLabel"><label for="register_country" class="label">Sắp Xếp: </label></td>
+                        <td>
+                          <select name="country" id="register_country" style="width:160px">
+                            <option value="india">india</option>
+                            <option value="pakistan">pakistan</option>
+                            <option value="africa">africa</option>
+                            <option value="china">china</option>
+                            <option value="other">other</option>
+                          </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <input type="submit" value="Lọc" >
+                    </tr>
+                </tr>
             </div>
-        </div>
-    </div>
-
-    <div class="slider">
-        <div class="contents">
-            <div class="title">
-                <h2>Window Bản Quyền</h2>
-            </div>
-            <div class="carts">
-                <?php
-                $sql = "SELECT id,image,name,price FROM products WHERE category_id=2 AND status = 0";
-                $result = mysqli_query($conn, $sql);
-
-                while ($kq = mysqli_fetch_assoc($result)) {
-                ?>
-                    <div class="card" style="width: 18rem; margin-bottom:20px;">
-                        <img src="<?php echo $kq['image']; ?>" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $kq['name']; ?></h5>
-                            <p class="card-text"><?php echo $kq['price']; ?><sup> đ</sup></p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-
-                <?php } ?>
-            </div>
-        </div>
-    </div>
-
-    <div class="slider">
-        <div class="contents">
-            <div class="title">
-                <h2>Window Bản Quyền</h2>
-            </div>
-            <div class="carts">
-                <?php
-                $sql = "SELECT id,image,name,price FROM products WHERE category_id=3 AND status = 0";
-                $result = mysqli_query($conn, $sql);
-
-                while ($kq = mysqli_fetch_assoc($result)) {
-                ?>
-                    <div class="card" style="width: 18rem; margin-bottom:20px;">
-                        <img src="<?php echo $kq['image']; ?>" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $kq['name']; ?></h5>
-                            <p class="card-text"><?php echo $kq['price']; ?><sup> đ</sup></p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-
-                <?php } ?>
+            <div class="carts" style="margin-top: 30px;">
             </div>
         </div>
     </div>
-
 
     <footer class="bg-dark text-white pt-5 pb-4">
         <div class="container text-center text-md-left">
