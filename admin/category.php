bb6d8746b1f7e15b4bff19cab18457497a477145
Vu tran <levu1962004@gmail.com>
2023-09-13 13:56:45 +0700

    init

--- admin/category.php
+++ admin/category.php
@@ -66,16 +66,15 @@ $conn->close();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="./css/search.css">
 </head>
 
 <body>
     <?php include('../admin/header.php') ?>
 
     <div class="slider">
-        <div class="contents">
+        <div class="contents" >
             
-            <div class="cards" style="width: 18rem; margin-bottom:20px;">
+            <div class="cards" style="width: 18rem; margin-bottom:20px;display: grid;grid-template-columns: repeat(4, 300px);">
                 <?php echo $render; ?>
             </div>
         </div>
--- admin/search.php
+++ admin/search.php
@@ -34,7 +34,6 @@ if (isset($_POST['name-product'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="./css/search.css">
 </head>
 
 <body>
